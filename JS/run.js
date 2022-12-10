968fd914677097122b691434e4e74698457dbcc8
aya <114641290+Ayanchi@users.noreply.github.com>
2022-12-10 14:59:47 +0600

    Add files via upload

--- JS/run.js
+++ JS/run.js
@@ -1,32 +1,33 @@
 
 
 class Player extends Phaser.Physics.Matter.Sprite{
-    constructor(data){
+        constructor(data){
         let{scene,x,y,texture} = data;
         super(scene.matter.world,x,y,texture);
         this.scene.add.existing(this);
 
-        const{Body,Bodies}=Phaser.Physics.Matter.Matter;
-        var playCollider = Bodies.circle(this.x, this.y, 19, {isSensor:false,label:'playCollider'});
-        var playSensor = Bodies.circle(this.x, this.y, 40, {isSensor:true,label:'playSensor'});
+       /* const{Body,Bodies}=Phaser.Physics.Matter.Matter;
+        var playCollider = Bodies.circle(this.x ,this.y, 50 , 80,{isSensor:false,label:'playCollider'});
+        var playSensor = Bodies.circle(this.x,this.y, 80, {isSensor:true,label:'playSensor'});
         const compoundBody = Body.create({
             parts:[playCollider,playSensor],
             frictionAir: 0.40
         })
-        this.setExistingBody(compoundBody);
+        this.setExistingBody(compoundBody);*/
         this.setFixedRotation();
+
     }
     static preload(scene){
         scene.load.atlas('little_name', 'myImg/little_name.png', 'myImg/little_name_atlas.json');
         scene.load.animation('little_name_anim', 'myImg/little_name_anim.json');
-        //let imgUP = scene.load.image('')
+       
     }
 
     get velocity(){
         return this.body.velocity;
     }
     update(){
-        const speed = 4;
+        const speed = 2;
         let playVelos = new Phaser.Math.Vector2();
         if(this.inputKeys.left.isDown){
             playVelos.x = -0.1;
@@ -73,7 +74,7 @@ class MainScene extends Phaser.Scene{
         this.matter.world.convertTilemapLayer(layer2);
 
 
-        this.player = new Player({scene:this, x:50, y:50, texture:'little_name'});
+        this.player = new Player({scene:this, x:100, y:100, texture:'little_name'});
         this.player.inputKeys = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
