7c327a25c2a13988412ba5479b684b58e2f1db32
aya <114641290+Ayanchi@users.noreply.github.com>
2022-12-22 16:00:52 +0600

    Add files via upload

--- JS/run.js
+++ JS/run.js
@@ -1,9 +1,11 @@
 
 
+
 class Player extends Phaser.Physics.Matter.Sprite{
-        constructor(data){
-        let{scene,x,y,texture} = data;
-        super(scene.matter.world,x,y,texture);
+    constructor(data) {
+        let{scene,x,y,texture,frame} = data;
+
+        super(scene.matter.world,x,y,texture,frame);
         this.scene.add.existing(this);
 
        /* const{Body,Bodies}=Phaser.Physics.Matter.Matter;
@@ -29,36 +31,32 @@ class Player extends Phaser.Physics.Matter.Sprite{
     update(){
         const speed = 2;
         let playVelos = new Phaser.Math.Vector2();
-        if(this.inputKeys.left.isDown){
+        if(this.inputKeys.left.isDown && this.x>=26){
             playVelos.x = -0.1;
-        } else if(this.inputKeys.right.isDown){
+            this.anims.play('walk', true);
+        } else if(this.inputKeys.right.isDown && this.x<=953){
             playVelos.x = 0.1;
+            this.anims.play('walk', true);
         }
-        if(this.inputKeys.up.isDown){
+        if(this.inputKeys.up.isDown && this.y>=40){
             playVelos.y = -0.1;
-        } else if(this.inputKeys.down.isDown){
+            this.anims.play('behind', true);
+        } else if(this.inputKeys.down.isDown && this.y<=558){
             playVelos.y = 0.1;
+            this.anims.play('face', true);
         }
-        if(Math.abs(this.velocity.y) == -1){
-            this.load.image('myImg/ayana2.png');
-        }else if(Math.abs(this.velocity.y) == 1){
-            this.load.image('myImg/ayana_behind.png');
-        }
-
-
+       
         playVelos.normalize();
         playVelos.scale(speed);
         this.setVelocity(playVelos.x, playVelos.y);
         
-        this.anims.play('walk', true);
-
     }
 }
 
 
 class MainScene extends Phaser.Scene{
     constructor(){
-        super('MainScene');
+        super('mainscene');
     }
     preload(){
         Player.preload(this);
@@ -80,14 +78,37 @@ class MainScene extends Phaser.Scene{
             down: Phaser.Input.Keyboard.KeyCodes.S,
             left: Phaser.Input.Keyboard.KeyCodes.A,
             right: Phaser.Input.Keyboard.KeyCodes.D,
+            top: Phaser.Input.Keyboard.KeyCodes.Z
         })
+        
+        //this.scene.stop('mainscene');
+        //this.scene.remove('MainScene');
+        //console.log(this.scene);
+        //this.scene.add('darkscene');
+        
     }
     update(){
         this.player.update();
     }
 }
 
-
+class DarkScene extends Phaser.Scene{
+    constructor(){
+        super('darkscene');
+    }
+    preload(){
+       /// this.load.image('tiles','myImg/RPG Nature Tileset.png');
+        //this.load.tilemapTiledJSON('map','myImg/darkmap.json');
+    }
+    create(){
+       /* const map = this.make.tilemap({key: 'map'});
+        const tileset = map.addTilesetImage('RPG Nature Tileset','tiles',32,32,0,0);
+        const layer1 = map.createLayer('Tile Layer 1', tileset,10,10);
+        const layer2 = map.createLayer('Tile Layer 2',tileset,10,10);
+        layer2.setCollisionByProperty({collides:true});
+        this.matter.world.convertTilemapLayer(layer2);*/
+    }
+}
 
 
 const config = {
@@ -96,7 +117,7 @@ const config = {
     backgroundColor: '#99999',
     type: Phaser.AUTO,
     parent: 'do_it',
-    scene: [MainScene],
+    scene: [MainScene, DarkScene],
     scale:{
         zoom:1
     },
